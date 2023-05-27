01f735179b42bd508eb3a86a82c62e53bb81a8d1
Fukji <littlenubboy@gmail.com>
2023-05-27 23:19:21 +0700

    Create schema & layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import SideBar from './SideBar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="h-screen">
+          <SideBar />
+          <div className="bg-white sm:ml-16">
+            {children}  
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
