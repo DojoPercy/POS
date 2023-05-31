88cb332c322899efb43d13a86ecc89d2dbed85e9
Fukji <littlenubboy@gmail.com>
2023-05-31 16:39:43 +0700

    overhaul transaction page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,35 +1,6 @@
-export function sortBy(a: any, b: any, columnName: string, desc: boolean) {
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-    const A = columnName.split('.').reduce((obj, currentKey) => {
-        if (obj && typeof obj === 'object' && currentKey in obj) {
-        return obj[currentKey];
-        }
-        return undefined;
-    }, a);
-
-    const B = columnName.split('.').reduce((obj, currentKey) => {
-        if (obj && typeof obj === 'object' && currentKey in obj) {
-            return obj[currentKey];
-        }
-        return undefined;
-    }, b);
-
-    if (A === undefined && B === undefined) {
-        return 0;
-    }
-    if (A === undefined) {
-        return (desc ? 1 : -1);
-    }
-    if (B === undefined) {
-        return (desc ? -1 : 1);
-    }
-    if (A > B) {
-        return (desc ? -1 : 1);
-    }
-    else if (A < B) {
-        return (desc ? 1 : -1);
-    }
-    else {
-        return 0;
-    }
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
 }
