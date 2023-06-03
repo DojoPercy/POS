63d20a661f551ba5506820b91d2337d29c9bb90c
Fukji <littlenubboy@gmail.com>
2023-06-03 21:28:50 +0700

    improve linechart rendering

--- components/ui/responsiveLineChart.tsx
+++ components/ui/responsiveLineChart.tsx
@@ -7,23 +7,22 @@ import {
     Tooltip,
     Legend,
     Line
-} from "recharts";
+} from "recharts"
 
 export function ResponsiveLineChart({ data, value }: { data: any, value: string }) {
     return (
         <div className="flex w-full font-semibold text-xs">
             <ResponsiveContainer width='100%' height={400}>
                 <LineChart data={data}>
-                {/* <CartesianGrid strokeDasharray="3 3" /> */}
                 <XAxis dataKey="date" stroke="#888888"
           fontSize={12}
           tickLine={false}/>
                 <YAxis fontSize={12}
           tickLine={false}/>
                 <Tooltip />
-                <Line type="monotone" dataKey={value} stroke="#18181b" strokeWidth={2} />
+                <Line type="monotone" dataKey={value} key={value} stroke="#18181b" strokeWidth={2} />
                 </LineChart>    
             </ResponsiveContainer>
         </div>
-    );
+    )
 }
