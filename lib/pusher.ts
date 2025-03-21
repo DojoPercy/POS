778b3b20571999dbf9f1a933cc08f48a9f4bd1c7
David Ojo <ojodavid115@gmail.com>
2025-03-21 12:15:27 +0000

    feat: update order update URL to use environment variable for base URL

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -2,7 +2,8 @@ import { OrderType } from "./types/types";
 
 export async function sendOrderUpdate(order: any){
     console.log("Sending order update:", order, process.env.NEXT_PUBLIC_BASE_URL);
-   const res = await fetch(`http://localhost:3000/api/pusher/orders`, {
+     const url =process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+   const res = await fetch(`${url}/api/pusher/orders`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(order),
